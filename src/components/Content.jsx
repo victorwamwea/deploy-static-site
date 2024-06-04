2c2e6dc28c917351aa138bf0ab27db6144ce093c
zablon <zablonoigo51@gmail.com>
2024-06-04 15:58:58 +0300

    List of component

---
+++ src/components/Content.jsx
@@ -0,0 +1,19 @@
+function Content() {
+  return (
+    <div className='max-w-4xl px-10 py-6 mx-auto bg-white'>
+        <div className="">
+            <h2 className="mb-3 text-4xl font-bold text-gray-800 uppercase">
+                profile
+            </h2>
+            <div className="">
+            <p className="text-lg font-light leading-8 text-gray-700">
+            Driven and enthusiastic Web Developer with a strong  passion for creating exceptional web experiences. Experienced in manual testing, test automation, tracking, tools, and A/B testing.
+            Quick leaners, team player, and effective communicator. Proficient in Continous Delivery toools. Familiar with Python and Django.
+        </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+import Content from './Content'
+import Header from './Header'
+function Resume() {
+  return (
+    <>
+    <section className="min-h-[100vh] bg-slate-100">
+        <Header/>
+        <Content/>
+    </section>
+    </>
+  )
+}
+
+export default Resume
