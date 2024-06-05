61f7c10954d2cd7800e032c3d46700db434ddce3
zablon <zablonoigo51@gmail.com>
2024-06-05 19:08:24 +0300

    Adding experinec content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -13,24 +13,37 @@ function Content() {
             </div>
         </div>
         <div className="">
-            <h2 className="mb-3 text-4xl font-bold text-gray-800 uppercase">
-                professional experience
-            </h2>
-            <div className="">
-            <div className="">
-              <p className="">
-                may 2018 - present
-                mountain view, california
-              </p>
-            </div>
-            <div className="">
-            <p className="text-lg font-light leading-8 text-gray-700">
-            Driven and enthusiastic Web Developer with a strong  passion for creating exceptional web experiences. Experienced in manual testing, test automation, tracking, tools, and A/B testing.
-            Quick leaners, team player, and effective communicator. Proficient in Continous Delivery toools. Familiar with Python and Django.
-        </p>
-            </div>
-            </div>
+    <h2 className="mb-3 text-4xl font-bold text-gray-800 uppercase">
+        professional experience
+    </h2>
+    <div className="grid grid-cols-2 gap-6 mb-6 place-items-center">
+        <div className="">
+            <p className="">
+                March 2019 - Present <br />
+                Mountain View, California
+            </p>
         </div>
+        <div className="">
+            <p className="text-lg font-light leading-snug text-gray-700">
+                Accomplished Web Developer with a passion for crafting high-quality web applications. Demonstrated expertise in manual and automated testing, tracking and analytics tools, and A/B testing methodologies. Recognized for quick learning, effective teamwork, and clear communication. Highly proficient in Continuous Delivery tools and experienced with Python and Django.
+            </p>
+        </div>
+    </div>
+    <div className="grid grid-cols-2 gap-6 place-items-center">
+        <div className="">
+            <p className="">
+                May 2018 - Feb 2019 <br />
+                Mary Hill, New York
+            </p>
+        </div>
+        <div className="">
+            <p className="text-lg font-light leading-snug text-gray-700">
+                Spearheaded the development of a high-traffic web application that boosted user engagement by 25%. Led a team of developers in implementing innovative A/B testing strategies, resulting in a 20% increase in conversion rates. Streamlined the deployment process by integrating Continuous Delivery tools, significantly reducing deployment time. Adept in using Python and Django to develop robust back-end solutions.
+            </p>
+        </div>
+    </div>
+</div>
+
     </div>
   )
 }
