a4dbdbfd7062cd8a21964945b12b0bd70db5e774
zablon <zablonoigo51@gmail.com>
2024-06-05 14:09:03 +0300

    Adding professional experience section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 function Content() {
   return (
     <div className='max-w-4xl px-10 py-6 mx-auto bg-white'>
-        <div className="">
+        <div className="mb-6">
             <h2 className="mb-3 text-4xl font-bold text-gray-800 uppercase">
                 profile
             </h2>
@@ -10,6 +10,25 @@ function Content() {
             Driven and enthusiastic Web Developer with a strong  passion for creating exceptional web experiences. Experienced in manual testing, test automation, tracking, tools, and A/B testing.
             Quick leaners, team player, and effective communicator. Proficient in Continous Delivery toools. Familiar with Python and Django.
         </p>
+            </div>
+        </div>
+        <div className="">
+            <h2 className="mb-3 text-4xl font-bold text-gray-800 uppercase">
+                professional experience
+            </h2>
+            <div className="">
+            <div className="">
+              <p className="">
+                may 2018 - present
+                mountain view, california
+              </p>
+            </div>
+            <div className="">
+            <p className="text-lg font-light leading-8 text-gray-700">
+            Driven and enthusiastic Web Developer with a strong  passion for creating exceptional web experiences. Experienced in manual testing, test automation, tracking, tools, and A/B testing.
+            Quick leaners, team player, and effective communicator. Proficient in Continous Delivery toools. Familiar with Python and Django.
+        </p>
+            </div>
             </div>
         </div>
     </div>
